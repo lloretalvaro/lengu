e2a260bd44d8a1e4743be74ed307698d0a821913
Alvaro Lloret Lopez <lloret@m1macbookair.local>
2022-04-17 18:34:05 +0200

    starting point app with basic functionalities

---
+++ Lengu/LenguApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenguApp.swift
+//  Lengu
+//
+//  Created by Alvaro Lloret Lopez on 17/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LenguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
